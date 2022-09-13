54b39adc8e0e893f5c272e4f477e6d26522a7e60
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-09-13 22:13:51 +0600

    add email commment

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-const cors = require('cors')
+const cors = require('cors');
+const { query } = require('express');
 
 app.use(cors());
 app.use(express.json());
@@ -12,34 +13,49 @@ const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
-    try{
+async function run() {
+    try {
         const socialCollection = client.db('allCollection').collection('post');
         const likeCollection = client.db('allCollection').collection('like');
         const commentCollection = client.db('allCollection').collection('comment');
+        const userCollection = client.db('allCollection').collection('user');
+        const profileCollection = client.db('allCollection').collection('profile');
 
         // post for data 
-        app.post('/post', async(req,res)=>{
+        app.post('/post', async (req, res) => {
             const post = req.body;
             const result = await socialCollection.insertOne(post);
             res.send(result);
         })
 
-        app.get('/post', async(req,res)=>{
-            const query ={};
+        app.get('/post', async (req, res) => {
+            const query = {};
             const cursor = socialCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         })
-        app.get('/post/:id', async(req,res)=>{
+        // get data email 
+        app.get('/data', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await socialCollection.find(query).toArray()
+            res.send(result);
+        })
+
+        app.get('/post/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const post = await socialCollection.findOne(query);
             res.send(post);
         })
 
+
+
+        // -------------
+
+
         // like for 
-        app.put('/like/:email', async(req,res)=>{
+        /* app.put('/like/:email', async(req,res)=>{
             const email = req.params.email;
             const user=req.body;
             const filter = {eamil:email};
@@ -49,32 +65,67 @@ async function run(){
             };
             const result = await likeCollection.updateOne(filter, updateDoc, options);
             res.send(result);
-        })
+        }) */
         // comment 
-        app.post('/comment', async(req,res)=>{
+        app.post('/comment', async (req, res) => {
             const comment = req.body;
             const result = await commentCollection.insertOne(comment)
             res.send(result);
         })
-        app.get('/comment', async(req,res)=>{
+        app.get('/comment', async (req, res) => {
             const id = req.query.id;
-            const query= {id:id};
+            const query = { id: id };
             const result = await commentCollection.find(query).toArray();
             res.send(result)
         })
 
+        // user for data 
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
+
+        // profile edit 
+           app.put('/profile/:email', async (req, res) => {
+               const email = req.params.email;
+               console.log(email)
+               const user = req.body;
+               console.log(user)
+               const filter = { email: email };
+               const options = { upsert: true };
+               const updateDoc = {
+                   $set: user,
+               };
+               const result = await profileCollection.updateOne(filter, updateDoc, options)
+               res.send(result);
+           })
+           app.get('/profile', async(req,res)=>{
+               const email = req.query.email;
+               const query = {email:email};
+               const result = await profileCollection.find(query).toArray()
+               res.send(result);
+           })
+
+
     }
-    finally{
+    finally {
 
     }
 
 }
 run().catch(console.dir);
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('connected')
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('Server is running');
 })
